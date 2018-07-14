a2b75d7970021f25de2cb93e27cf4250d77a2d9a
Thiago Talma <thiago@thiagomt.com>
2018-07-13 21:30:22 -0300

    diff-so-fancy

--- git/install.sh
+++ git/install.sh
@@ -7,6 +7,11 @@ else
 	git config --global credential.helper cache
 fi
 
+rm -f /tmp/diff-so-fancy
+curl -s -L -o /tmp/diff-so-fancy https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy
+chmod +x /tmp/diff-so-fancy
+sudo mv -f /tmp/diff-so-fancy /usr/local/bin/diff-so-fancy
+
 # better diffs
 if which diff-so-fancy >/dev/null 2>&1; then
 	git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
