de36ff0596194c0692b5d3a7123d21964003edde
Thiago Talma <thiago@thiagomt.com>
2018-07-14 00:09:17 -0300

    novos plugins

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,19 +1,14 @@
 #!/bin/sh
-if which hub >/dev/null 2>&1; then
-	alias git='hub'
-fi
 
 alias gl='git pull --prune'
 alias glg="git log --graph --decorate --oneline --abbrev-commit"
 alias glga="glg --all"
 alias gp='git push origin HEAD'
 alias gpa='git push origin --all'
-alias gd='git diff'
 alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
 alias gb='git branch -v'
-alias ga='git add'
 alias gaa='git add -A'
 alias gcm='git commit -m'
 alias gcam='git commit -a -m'
@@ -26,6 +21,3 @@ if which svu >/dev/null 2>&1; then
 	alias gtminor='echo `svu m`; git tag `svu m`'
 fi
 
-gi() {
-	curl -s "https://www.gitignore.io/api/$*"
-}
