dcad0777b959299e810f5079d01e3f43a0d6f63b
Thiago Talma <thiago@thiagomt.com>
2018-07-14 18:46:25 -0300

    marker

---
+++ marker/install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+if [ ! -d /usr/local/opt/marker ]; then
+  git clone --quiet --depth 1 https://github.com/pindexis/marker /usr/local/opt/marker
+else
+  git --work-tree /usr/local/opt/marker fetch
+  git --work-tree /usr/local/opt/marker reset --quiet --hard @{u}
+fi
+
+./usr/local/opt/install.py
