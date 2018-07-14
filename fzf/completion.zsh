545cf7f3e5e0baf22a7acb5471bfffe2119a3ac1
Thiago Talma <thiago@thiagomt.com>
2018-07-13 23:23:20 -0300

    zlsun/solarized-man e commando-not-found

--- fzf/completion.zsh
+++ fzf/completion.zsh
@@ -3,11 +3,12 @@ test -d /usr/local/opt/fzf/shell || return 0
 
 export PATH="$PATH:/usr/local/opt/fzf/bin"
 
-export FZF_COMPLETION_TRIGGER='**'
+export FZF_COMPLETION_TRIGGER='~~'
 
-export FZF_DEFAULT_COMMAND='fd --type file --color=always --follow --hidden --exclude .git'
+export FZF_DEFAULT_COMMAND='fd --type file --follow --hidden --exclude .git'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_COMPLETION_OPTS='+c -x'
+export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
+export FZF_COMPLETION_OPTS='--exit-0'
 
 # Use fd (https://github.com/sharkdp/fd) instead of the default find
 # command for listing path candidates.
