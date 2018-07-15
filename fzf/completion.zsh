6c375a2eeea91e0cde63006cb8be1b245640d3a0
Thiago Talma <thiago@thiagomt.com>
2018-07-14 23:28:33 -0300

    path

--- fzf/completion.zsh
+++ fzf/completion.zsh
@@ -1,18 +1,6 @@
 #!/bin/sh
 test -d /usr/local/opt/fzf/shell || return 0
 
-export PATH="$PATH:/usr/local/opt/fzf/bin"
-
-export FZF_COMPLETION_TRIGGER='~~'
-
-export FZF_DEFAULT_COMMAND='fd --type file --follow --hidden --exclude .git'
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
-export FZF_COMPLETION_OPTS='--exit-0'
-
-# wfxr/formarks
-export FZF_MARKS_JUMP='^b'
-
 # Use fd (https://github.com/sharkdp/fd) instead of the default find
 # command for listing path candidates.
 # - The first argument to the function ($1) is the base path to start traversal
