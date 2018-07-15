c08caec9f0434ae7c98f4e435bb75cadeea03e94
Thiago Talma <thiago@thiagomt.com>
2018-07-14 22:01:20 -0300

    alguns ajustes

--- vim/install.sh
+++ vim/install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+curl -fLso ~/.vim/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim +PlugInstall </dev/tty
-vim +PlugUpdate </dev/tty
+vim +PlugInstall +qall </dev/tty
+vim +PlugUpdate +qall </dev/tty
 
 if which neovim >/dev/null 2>&1; then
-	curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+	curl -fLso ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 	mkdir -p ~/.config/nvim/
